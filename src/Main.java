bad27f1ad572535e317254909460090439d9bbc1
周第文 <zhoudiwen@zhoudiwen-2.local>
2017-04-28 16:39:07 +0800

    功能1开始开发

--- src/Main.java
+++ src/Main.java
@@ -7,4 +7,6 @@ public class Main {
 
         System.out.println("first commit devekop");
     }
+
+
 }
---
+++ src/Test1.java
@@ -0,0 +1,8 @@
+/**
+ * Created by zhoudiwen on 17/4/28.
+ */
+public class Test1 {
+    public void test1() {
+        System.out.println("create test1 and first commit");
+    }
+}
